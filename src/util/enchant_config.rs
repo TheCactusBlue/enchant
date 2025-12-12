cd5c005880f29f36a964c6bc34d58124f423c374
CactusBlue <thecactusblue@gmail.com>
2025-12-11 22:44:25 -0800

    aaa

--- src/util/enchant_config.rs
+++ src/util/enchant_config.rs
@@ -2,6 +2,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Default, Clone, Debug, Serialize, Deserialize)]
 pub struct EnchantJson {
+    pub permissions: Permissions,
+}
+
+#[derive(Default, Clone, Debug, Serialize, Deserialize)]
+pub struct Permissions {
     pub bash: BashConfig,
 }
 
